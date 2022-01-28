9d8b3c638fd06b5343dc69bf4e66bb1019afc97a
Dmitry <libafer99@gmail.com>
2022-01-28 16:49:14 +0500

    first commit

---
+++ gulp/tasks/fonts.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp');
+const ttf2woff = require('gulp-ttf2woff');
+const ttf2woff2 = require('gulp-ttf2woff2');
+
+module.exports.copyFonts = function copyFonts() {
+    return gulp.src(['src/**/*.{ttf, woff2, woff}']).pipe(gulp.dest('build/'));
+};
+
+module.exports.convertFonts = function convertFonts() {
+    return gulp
+        .src(['src/**/*.ttf', '!src/**/_*.ttf'])
+        .pipe(ttf2woff2())
+        .pipe(gulp.dest('src/'))
+        .pipe(gulp.src(['src/**/*.ttf', '!src/**/_*.ttf']))
+        .pipe(ttf2woff())
+        .pipe(gulp.dest('src/'));
+};
