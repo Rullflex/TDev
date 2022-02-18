b38cb6d858b29a8beea4b985548ba905c6226305
Dmitry <libafer99@gmail.com>
2022-02-18 15:20:50 +0500

    fix: slider words on desktop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,19 +2,34 @@ import UIkit from 'uikit';
 import { app, form } from './_core';
 import sectionServicesScript from './_s3';
 
-const { ready, $, $$, play, pause, mute } = UIkit.util;
+const { ready, $, $$ } = UIkit.util;
 
 ready(function () {
     app.init();
     form.init('form');
 
     let isDesktop;
+    let slider;
 
     app.matchMediaListener(
         app.lg,
-        () => (isDesktop = false),
-        () => (isDesktop = true),
+        () => {
+            isDesktop = false;
+            sliderHandler(false);
+        },
+        () => {
+            isDesktop = true;
+            sliderHandler(true);
+        },
     );
 
     sectionServicesScript();
+
+    function sliderHandler(isDesktop) {
+        if (isDesktop) {
+            slider?.$destroy();
+        } else {
+            slider = UIkit.slider('.s4__slider', { finite: true });
+        }
+    }
 });
