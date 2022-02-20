b22bbfc2f07b19b9dab9cc0afb5e099734e197c4
Dmitry <libafer99@gmail.com>
2022-02-20 15:24:37 +0500

    add: cases page & services click logic

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,5 @@
 import UIkit from 'uikit';
 import { app, form } from './_core';
-import sectionServicesScript from './_s3';
 
 const { ready, $, $$ } = UIkit.util;
 
@@ -23,7 +22,14 @@ ready(function () {
         },
     );
 
-    sectionServicesScript();
+    const callbackModal = $('#callback');
+    const callbackModalContextInput = callbackModal.querySelector('input[name="context"]');
+
+    callbackModal.addEventListener('hidden', () => (callbackModalContextInput.value = ''));
+
+    $$('[data-uk-toggle="#callback"]').forEach((el) => {
+        el.addEventListener('click', () => (callbackModalContextInput.value = el.dataset.context ?? ''));
+    });
 
     function sliderHandler(isDesktop) {
         if (isDesktop) {
@@ -32,4 +38,40 @@ ready(function () {
             slider = UIkit.slider('.s4__slider', { finite: true });
         }
     }
+
+    // Section 3
+    (function () {
+        if (!document.querySelector(`.s3`)) {
+            return;
+        }
+
+        const sectionNode = document.querySelector('#section-services');
+        const starNode = sectionNode.querySelector('.s3__preview-star');
+        const imagesSet = sectionNode.querySelectorAll('.s3__preview-image');
+
+        const mouseMove = (event) => {
+            let y = event.offsetY;
+            starNode.style.cssText = `transform: rotate(${y}deg)`;
+        };
+
+        document.querySelectorAll('.s3__breadcrumb-link').forEach((el, index, set) => {
+            const updateActiveIndex = (index = -1) => {
+                app.changeActivitySet(set, index);
+                app.changeActivitySet(imagesSet, index);
+            };
+
+            el.addEventListener('mouseenter', () => updateActiveIndex(index));
+            el.addEventListener('mouseleave', updateActiveIndex);
+        });
+
+        app.matchMediaListener(
+            app.lg,
+            () => {
+                sectionNode?.removeEventListener('mousemove', mouseMove);
+            },
+            () => {
+                sectionNode?.addEventListener('mousemove', mouseMove);
+            },
+        );
+    })();
 });
